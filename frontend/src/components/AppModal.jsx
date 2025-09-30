457f46d6cf3cb3c5a659c2c513e92057835f9b8a
Josecau2 <joseca@symmetricalwolf.com>
2025-09-30 01:36:03 -0400

    AI: standard modal wrapper + mobile full-screen [step-7]

---
+++ frontend/src/components/AppModal.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  useColorModeValue,
+} from '@chakra-ui/react'
+
+const AppModal = ({
+  isOpen,
+  onClose,
+  title,
+  children,
+  footer,
+  size = { base: 'full', md: 'md' },
+  scrollBehavior = 'inside',
+  ...props
+}) => {
+  const overlayBg = useColorModeValue('blackAlpha.600', 'blackAlpha.600')
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size={size}
+      scrollBehavior={scrollBehavior}
+      isCentered
+      {...props}
+    >
+      <ModalOverlay bg={overlayBg} />
+      <ModalContent borderRadius="lg">
+        {title && (
+          <ModalHeader borderBottomWidth="1px">
+            {title}
+          </ModalHeader>
+        )}
+        <ModalCloseButton />
+        <ModalBody>
+          {children}
+        </ModalBody>
+        {footer && (
+          <ModalFooter>
+            {footer}
+          </ModalFooter>
+        )}
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default AppModal
